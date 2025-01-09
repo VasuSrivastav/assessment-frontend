5aafb2955ee4315f5e8b47f9a5d6a45acf71a7f3
VasuSri <vasusrivastav1@gmail.com>
2025-01-09 16:47:19 +0530

    zustand one line

--- src/store/zustand.js
+++ src/store/zustand.js
@@ -4,7 +4,7 @@ import toast from 'react-hot-toast';
 
 export const useStore = create((set, get) => ({
     authUser: null,
-    userRole: null,
+    userRole: "user",
     isSigningUp: false,
     isLoggingIn: false,
     isCheckingAuth: true,
